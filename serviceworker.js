bb1ae5bf64fad8d1d0bf5da4379faabd82d4b2b2
scipn <60628036+scipn@users.noreply.github.com>
2020-06-23 23:39:29 -0400

    update

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-var cacheName = 'gih-cache';
+var cacheName = 'gih-cache-v2';
 var CACHED_URLS = [
   '/',
   '/index.html',
@@ -15,16 +15,32 @@ self.addEventListener('install', function(e) {
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-     return caches.match(event.request).then(function(response) {
-        if (response) {
-          return response;
-        } else if (event.request.headers.get("accept").includes("text/html")) {
-          return caches.match("/index-offline.html");
-        }
-      });
-    })
-  );
+self.addEventListener("fetch", function(event) {
+  var requestURL = new URL(event.request.url);
+  if (requestURL.pathname === "/" || requestURL.pathname === "/index.html") {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match("/index.html").then(function(cachedResponse) {
+          var fetchPromise =
+            fetch("/index.html")
+            .then(function(networkResponse) {
+              cache.put("/index.html", networkResponse.clone());
+              return networkResponse;
+            });
+          return cachedResponse || fetchPromise;
+        });
+      })
+    );
+  } else if (
+    CACHED_URLS.includes(requestURL.href) ||
+    CACHED_URLS.includes(requestURL.pathname)
+  ) {
+    event.respondWith(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+        });
+      })
+    );
+  }
 });
