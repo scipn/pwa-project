9f0fe738dc3647451400b70805e20f34f70c5252
scipn <60628036+scipn@users.noreply.github.com>
2020-06-23 22:58:48 -0400

    cleaning

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,31 @@
-self.addEventListener("fetch", function(event) {
-  if (event.request.url.includes("/images/usf.png")) {
-    event.respondWith(
-      fetch("/images/usf.png")
-    );
-  }
+var cacheName = 'gih-cache';
+var CACHED_URLS = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+     return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          return caches.match("/index-offline.html");
+        }
+      });
+    })
+  );
 });
