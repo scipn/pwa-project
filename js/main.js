957e2f27bbea3bd87aa317bfef65d2a41e34d3d8
totally-not-michael <67381574+totally-not-michael@users.noreply.github.com>
2020-07-20 02:55:08 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -214,7 +214,7 @@ var options = {
     'Authorization' : 'key=AAAAVI0FpWQ:APA91bGyHQ-G1DxpJ6-dqQ7gU8VqtjjBEF4WoIplXLPb5VaNrOfvuSqFEaTwLem5AcAs1pVwF5Mttde8zWUuzI7r37i5TwlPQV5NlgYKcpN4DIkgxnl9_qGGwzjWEUFlw762fJ6-Sh2k'
 }
   };
-
+  
 var req = https.request(options, function(res) {
     console.log("statusCode: ", res.statusCode);
     console.log("headers: ", res.headers);
